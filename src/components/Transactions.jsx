507764dabfd8d460b807bc35f5736a97b44892ee
chariisss <charismakurniawan@gmail.com>
2022-07-30 22:43:39 +0700

    first commit

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,4 @@
+function Transactions() {
+  return <div>Transactions</div>;
+}
+export default Transactions;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x39f4D8c41C34275775408A58D7177484C3A0f4cf";
